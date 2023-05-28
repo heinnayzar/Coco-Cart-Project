02d9b9e78c5a33831ae5da3993bb0732aa1c0456
heinnayzar <129826440+heinnayzar@users.noreply.github.com>
2023-05-28 21:36:32 +0630

    second commit

--- dist/app.js
+++ dist/app.js
@@ -3,6 +3,20 @@ const faqTitle = document.querySelector('.faq-title')
 const faqContent = document.querySelector('.faq-content')
 const faqIcon = document.querySelector('.faq-icon')
 
+const burger = document.querySelector('.burger')
+const dropdownMenu = document.querySelector('.dropdown')
+const nav =document.querySelector('nav')
+
+burger.addEventListener('click', () => {
+    if (dropdownMenu.classList.contains('hidden')){
+        dropdownMenu.classList.remove('hidden')
+        nav.style.backgroundColor = 'white'
+    } else {
+        dropdownMenu.classList.add('hidden')
+        nav.style.backgroundColor = '#FFBB00'
+    }
+})
+
 // faqTitle.onclick = () => {
 //     faqContent.classList.toggle('show')
 //     faqIcon.classList.toggle('show')
